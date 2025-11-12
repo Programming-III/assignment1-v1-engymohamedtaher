5372507d59efffb9a1277544909ef3c7a905b3b3
engymohamedtaher <engy.mohamed.taher@gmail.com>
2025-11-12 15:13:52 +0200

    Update Visitor.h

--- Visitor.h
+++ Visitor.h
@@ -1,15 +1,20 @@
 #ifndef VISITOR_H
 #define VISITOR_H
-
-#include <string>
+#include<iostream>
+#include<string>
 using namespace std;
 
-//define visitor class here
-
-
-
-
-
-
-
+class Visitor{
+    private:
+        string vname;
+        int ticketsBought;
+    public:
+        Visitor();
+        Visitor(string vmane, int ticketsBought);
+        ~Visior();
+        void displayInfo();
+        string getVisitorName();
+        int getTicketsBought();
+        
+};
 #endif
