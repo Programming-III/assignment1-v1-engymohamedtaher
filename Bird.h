dd7a7bae0e0591da6cc694d1814d1587f857025b
engymohamedtaher <engy.mohamed.taher@gmail.com>
2025-11-12 15:10:08 +0200

    Update Bird.h

--- Bird.h
+++ Bird.h
@@ -1,14 +1,15 @@
 #ifndef BIRD_H
 #define BIRD_H
+#include<iostream>
+#include<string>
+using namespace std;
 
-#include "Animal.h"
-
-//define class bird here 
-
-
-
-
-
-
-
+class Bird: public Animal{
+    private:
+        float wingSpan;
+    public:
+        Bird();
+        Bird(string name, int age, bool isHungry, float wingSpan);
+        ~Bird();
+};
 #endif
