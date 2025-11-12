28a741cbf505dc3627e7608d8aa42f2d9e4d941b
engymohamedtaher <engy.mohamed.taher@gmail.com>
2025-11-12 15:54:56 +0200

    Define Reptile class with constructors and member variables
    Defined the Reptile class that inherits from Animal and added member variables and constructors.

--- Reptile.h
+++ Reptile.h
@@ -1,16 +1,15 @@
 #ifndef REPTILE_H
 #define REPTILE_H
+#include<iostream>
+#include<string>
+using namespace std;
 
-#include "Animal.h"
-
-//define reptile class here
-
-
-
-
-
-
-
-
-
+class Reptile: public Animal{
+    private:
+        bool isVenomous;
+    public:
+        Reptile();
+        Reptile(string name, int age,bool isVenomous, bool isHungry,bool isVenomous);
+        ~Reptile();
+};
 #endif
